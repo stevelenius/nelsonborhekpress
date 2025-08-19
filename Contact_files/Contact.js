1e13e5695019d0a67381586fb12b665080e8297e
stevelenius <44826562+stevelenius@users.noreply.github.com>
2025-08-19 14:25:34 -0500

    Add files via upload
    subdirectories

---
+++ Contact_files/Contact.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140703
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_files/ContactMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Publishers_Message_files/Publishers_Message.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140703
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Publishers_Message_files/Publishers_MessageMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
